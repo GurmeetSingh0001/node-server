d142422f7df9a0057d16bfd5bc4ef89bb62244e9
webexpert5 <nitinchauhan.odk@gmail.com>
2023-08-24 16:23:10 +0530

    server

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-import express from "express"
+const express = require("express");
 
 const app = express();
 const PORT = 4000;
@@ -16,3 +16,4 @@ app.get("/about", (req, res) => {
 });
 
 // Export the Express API
+module.exports = app;
